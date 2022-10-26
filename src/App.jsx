cd3eb14f9a97e84de1ed5d6bb67dba518bf90766
Cynthia <Liangshao2023@u.northwestern.edu>
2022-10-26 15:30:00 -0500

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header.jsx'
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
+import Homepage from './components/Homepage.jsx';
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -9,6 +11,15 @@ const App = () => {
   return (
     <div className="App">
       <Header/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={
+            <div>
+              <Homepage />
+            </div>
+          } />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+
+const Homepage = () => {
+  
+    return( 
+        <div className="container">
+            homepage
+        </div>  
+    );
+}
+
+export default Homepage;
