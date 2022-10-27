1de04dc55aa4e070a28d8757bdc91028bc4cdae4
Cynthia <Liangshao2023@u.northwestern.edu>
2022-10-27 15:31:12 -0500

    add dummy data to card

--- src/components/RatingCard.jsx
+++ src/components/RatingCard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Rating } from 'react-simple-star-rating'
-
+import { TextField } from "@mui/material";
 import './RatingCard.css';
 
 // const formatPrice = new Intl.NumberFormat([], { style: 'currency', currency: 'USD' }).format;
@@ -16,13 +16,17 @@ const RatingCard = ({data}) => {
     
     return (
         <div className="card m-1 p-2">
-            {/* <img src={data.thumbnail} className="card-img-top"/> */}
+            <div>
+                <img src={data.album.image_url} className="card-img-top"/>
+            </div>
             <div className="card-body">
-            <h5 className="card-title">title</h5>
-            <p className="card-text">artist</p>
+            <h5 className="card-title">{data.name}</h5>
+            <p className="card-text">{data.artist.name}</p>
             <Rating
                 onClick={handleRating}
             />
+
+            <TextField className="comment" label="Comment" variant="outlined" />
 
             {/* <p className="card-text">{formatPrice(product.price)}</p> */}
             </div>
