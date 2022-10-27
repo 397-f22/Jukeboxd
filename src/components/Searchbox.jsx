6a9f894356f59359971d56ae3005f093b4b24018
OwenJanssen <owenjanssen2023@u.northwestern.edu>
2022-10-27 15:47:14 -0500

    Search Results

--- src/components/Searchbox.jsx
+++ src/components/Searchbox.jsx
@@ -4,9 +4,10 @@ import { useState } from 'react';
 
 const Searchbox = () => {
     const [searchTerm, setSearchTerm] = useState('');
+    const [selectedSong, setSelectedSong] = useState(undefined);
+    const [data, setData] = useState(undefined);
 
     const updateSearchTerm = (event) => {
-
         setSearchTerm(event.target.value);
     }
 
@@ -15,14 +16,22 @@ const Searchbox = () => {
             .then(response => response.json())
             .then(data => {
                 console.log(data);
+                setData(data);
             });
-
     }
 
     return (
         <div className="container">
             <input type="text" placeholder="Search..." value={searchTerm} onChange={updateSearchTerm}></input>
             <button className='button-class' onClick={search}>Search</button>
+            {data && data.map(song => {
+                <div className={song === selectedSong ? "selected-song" : "unselected-song"} onClick={() => setSelectedSong(song)}>
+                    <div>{song.name}</div>
+                    <div>{song.artist.name}</div>
+                    <div>{song.album.name}</div>
+                    <div>{song.image.image_url}</div>
+                </div>
+            })}
         </div>
     );
 }
