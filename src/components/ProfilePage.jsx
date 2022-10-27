14389183387df58f5585541c6889f8adc71bd398
sid runwal <sidharthrunwal2022@u.northwestern.edu>
2022-10-27 15:29:42 -0500

    added spotify search

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,12 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 import RatingCard from './RatingCard.jsx';
+import Searchbox from './Searchbox.jsx';
 
 const ProfilePage = () => {
   
     return( 
         <div className="container">
             <RatingCard />
+            <Searchbox/>
 
 
 
