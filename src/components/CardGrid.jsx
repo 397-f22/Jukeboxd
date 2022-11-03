2fa0bff565ee8f2c6828a86e1119992f5daa2d43
OwenJanssen <owenjanssen2023@u.northwestern.edu>
2022-11-03 15:06:50 -0500

    styling for new profile/card updates

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -5,15 +5,14 @@ import "./CardGrid.css"
 
 
 const CardGrid = ({listOfRatingData, id}) => {
-    return listOfRatingData && <div className="card-grid">
+    return listOfRatingData ? <div className="card-grid">
       <NewRatingCard data={listOfRatingData} id={id} newRatingId={listOfRatingData.length}/>
-
       {
         listOfRatingData.map((songData, i) => 
           <RatingCard data={songData} id={id} index={i} key={i}/>
         )
       }
-    </div>
+    </div> : <NewRatingCard data={listOfRatingData} id={id} newRatingId={0}/>
   };
   
   export default CardGrid;
