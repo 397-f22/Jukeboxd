f12e30df3bfcab348639627abbc377342c33282b
OwenJanssen <67287024+OwenJanssen@users.noreply.github.com>
2022-11-06 18:31:35 -0600

    Refactoring

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -4,75 +4,36 @@ import NewRatingCard from './NewRatingCard.jsx';
 import "./CardGrid.css"
 import RatingCardFriend from './RatingCardFriend.jsx';
 import { useDbData, useDbUpdate } from '../utilities/firebase';
-import { FlashOnRounded } from '@mui/icons-material';
+import SubscribeButton from './SubscribeButton.jsx';
 
 // id is from the url of the page, user 
-const CardGrid = ({ data, user, id }) => {
-  const listOfRatingData = data ? data[id].reviews : undefined;
-  const [update, result] = useDbUpdate(`/${user.id}`);
-
-  const subscribeToProfile = () => {
-    console.log(id, !data[user.id].subscriptions);
-    if (!data[user.id].subscriptions) {
-      update(
-        {"subscriptions": [id]}
-      );       
-    }
-    else {
-      update(
-        {"subscriptions": [id, ...data[user.id].subscriptions]}
-      );  
-    }
-  };
-
-  const unsubscribeToProfile = () => {
-    // remove current page's id from your subscriptions in db
-    const new_subscriptions_list = data[user.id].subscriptions.filter((element) => {element !== id})
-    console.log("new subs:", new_subscriptions_list.length > 0 ? true : false)
-    update(
-      {"subscriptions": new_subscriptions_list.length > 0 ? new_subscriptions_list : ""}
-    );  
-  }
-
-  const isSubscribedTo = data[user.id].subscriptions.includes(id);
-  console.log("USER ID", user.id);
-  console.log("DATA [ USER ID ]", data[user.id]);
-  console.log(isSubscribedTo);
-
-  return id === user.id ? 
+const CardGrid = ({ data, user, id: pageID }) => {
+  const listOfRatingData = data[pageID].reviews;
+  
+  return pageID === user.id ? 
     // if it's your page
-    <div> {listOfRatingData ?
-      <div className="card-grid">
-        <NewRatingCard data={listOfRatingData} id={id} newRatingId={listOfRatingData.length}/>
+    <div className="card-grid">
+      <NewRatingCard data={listOfRatingData} id={pageID} newRatingId={listOfRatingData.length}/>
 
-        {listOfRatingData.map((songData, i) =>
-          <RatingCard data={songData} id={id} index={i} key={i} />)}
-      </div> : 
-
-      <NewRatingCard data={listOfRatingData} id={id} newRatingId={0} />}
-    </div> :
+      {listOfRatingData && listOfRatingData.map((songData, i) =>
+        <RatingCard data={songData} id={pageID} index={i} key={i} />)}
+    </div> : 
 
     // viewing someone else's page
     <div>
       <div className="friend-profile-header">
-        <h1 className="text">{`${data[id].displayName}'s Profile`}</h1>
+        <h1 className="text">{`${data[pageID].displayName}'s Profile`}</h1>
         {/* switch between subscribe and unsub buttons, based on whether subscribed */}
-        {user && 
-          <div>
-            {isSubscribedTo 
-              ? <button className="btn btn-danger" onClick={unsubscribeToProfile}>Unsubscribe</button>
-              : <button className="btn btn-success" onClick={subscribeToProfile}>Subscribe</button>}
-          </div>
-        }
+        {user ? <SubscribeButton data={data} user={user} pageID={pageID} /> : <div/>}
       </div>
 
       {listOfRatingData && <div>
         <div className="card-grid">
           {listOfRatingData.map((songData, i) =>
-            <RatingCardFriend data={songData} id={id} index={i} key={i} />)}
+            <RatingCardFriend data={songData} id={pageID} index={i} key={i} />)}
         </div>
       </div>}
-    </div>    
+    </div>
 };
 
 export default CardGrid;
--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -5,7 +5,7 @@ import { useDbData, useDbUpdate } from '../utilities/firebase.js';
 
 const ProfilePage = ({id, user, data}) => {
     // if db has data for both your id and the page's id, show the profile page
-    return (data && (data[id] && data[user.id])) && <div className="container" style={{ marginTop: "2rem", }}>
+    return (data && (data[id] && (!user || data[user.id]))) && <div className="container" style={{ marginTop: "2rem", }}>
         <CardGrid user={user} data={data} id={id} />
         <div style={{ height: "100px" }} />
     </div>;
