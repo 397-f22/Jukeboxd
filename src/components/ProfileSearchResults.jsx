bfe46f56414fa2098afd3eabc56fa7789cf64cfe
Cynthia <Liangshao2023@u.northwestern.edu>
2022-11-11 12:00:17 -0600

    bug fix

--- src/components/ProfileSearchResults.jsx
+++ src/components/ProfileSearchResults.jsx
@@ -4,17 +4,15 @@ import { useState } from "react";
 const getUser = (userId, spotify) => {
     const [url, setUrl] = useState("");
     spotify.getUser(userId).then((user) => {
-        setUrl(user.images[0].url);
-        console.log(user)
-        console.log(user.images[0].url)
+        setUrl(user.images.length > 0 ? user.images[0].url : "");
     }).catch((err) => {
 
     })
+    
     return url;
 }
 
 const ProfileSearchResults = ({results, spotify}) => {
-    console.log()
     if (!results) {
         return <div>Data is loading...</div>;
     }
@@ -23,13 +21,15 @@ const ProfileSearchResults = ({results, spotify}) => {
     }
 
     return <div className='profile-search-results'>
-        {results.map((profile, i) => 
-            <div className='profile' key={i}>
-                <img className="profile-image" src={getUser(profile.userId, spotify)}></img>
+        {results.map((profile, i) => {
+            const profilePicture = getUser(profile.userId, spotify);
+            const defaultPicture = "https://i.pinimg.com/originals/f4/ad/6b/f4ad6b120817d2eecae26e07d415de97.jpg";
+            return <div className='profile' key={i}>
+                <img className="profile-image" src={profilePicture !== "" ? profilePicture : defaultPicture}></img>
                 <span className="profile-name">{profile.displayName}</span>
                 <Button variant="outlined" color="success" onClick={()=>goToProfile(profile)}>Go To Profile</Button>
             </div>
-        )}
+        })}
     </div>;
 }
 
